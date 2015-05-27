327459c25cd91e6044578a51273ac6ff5b5d281b
Doug Theobald <doug.theobald@metaswitch.com>
2015-05-27 04:22:36 -0700

    Use the right release (release-5-8-1) (rebase to release-73-pre0)

---
+++ clearwater.mk
@@ -0,0 +1,15 @@
+DEB_COMPONENT := clearwater-monit
+DEB_MAJOR_VERSION := 5.8${DEB_VERSION_QUALIFIER}
+DEB_NAMES := clearwater-monit
+
+include build-infra/cw-deb.mk
+
+.PHONY: deb-build-monit
+deb-build-monit:
+	echo -e "${DEB_COMPONENT} (${DEB_VERSION}) unstable; urgency=low\n" >debian/changelog
+	echo -e "  * build from revision $$(git rev-parse HEAD)\n" >>debian/changelog
+	echo -e " -- $(CW_SIGNER_REAL) <$(CW_SIGNER)>  $$(date -R)\n" >>debian/changelog
+	debuild --no-lintian -b -uc -us
+
+.PHONY: deb
+deb: deb-build-monit deb-move
